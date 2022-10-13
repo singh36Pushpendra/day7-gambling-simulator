9bba9cae5a5d7d5cf825e9041f25afe6f394c788
singh36Pushpendra <psthakur.1999@gmail.com>
2022-10-13 12:29:53 +0530

    Added: UC2 First bet play!

--- src/gambler/program/GamblingSimulator.java
+++ src/gambler/program/GamblingSimulator.java
@@ -1,13 +1,31 @@
 package gambler.program;
 
+import java.util.Random;
+
 public class GamblingSimulator {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator Program!");
-        final int $STAKE_PER_DAY = 100, $PER_GAME = 1;
+        final int $PER_GAME = 1, WIN = 1, LOOSE = 0;
+        int stakePerDay = 100;
         
-        System.out.println("Gambler everyday stake: $" + $STAKE_PER_DAY + "\n"
+        System.out.println("Gambler everyday stake: $" + stakePerDay + "\n"
                 + "Gambler bet every game: $" + $PER_GAME);
+        
+        Random random = new Random();
+        int gamePoint = random.nextInt(2);
+        
+        switch(gamePoint) {
+            case WIN:
+                stakePerDay++;
+                System.out.println("Gambler WON!");
+                break;
+            case LOOSE:
+                stakePerDay--;
+                System.out.println("Gambler LOOSE!");
+        }
+        
+        System.out.println("Gambler stake after first play: " + stakePerDay);
     }
     
 }
