bb4d61a25ab8a6a24f2d21993c2b7c7abf32cd99
singh36Pushpendra <psthakur.1999@gmail.com>
2022-10-13 11:05:20 +0530

    Added: First commit

---
+++ src/gambler/program/GamblingSimulator.java
@@ -0,0 +1,13 @@
+package gambler.program;
+
+public class GamblingSimulator {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulator Program!");
+        final int $STAKE_PER_DAY = 100, $PER_GAME = 1;
+        
+        System.out.println("Gambler everyday stake: $" + $STAKE_PER_DAY + "\n"
+                + "Gambler bet every game: $" + $PER_GAME);
+    }
+    
+}
