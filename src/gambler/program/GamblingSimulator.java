eac3e8a398367810efd55b705bcbf171100be165
singh36Pushpendra <psthakur.1999@gmail.com>
2022-10-13 16:07:39 +0530

    Added: UC4 Everyday total stake Won|Lost

--- src/gambler/program/GamblingSimulator.java
+++ src/gambler/program/GamblingSimulator.java
@@ -13,24 +13,31 @@ public class GamblingSimulator {
                 + "Gambler bet every game: $" + $PER_GAME);
 
         Random random = new Random();
-        int gamePoint;
+        int gamePoint, lost, count;
 
-        int todayStake = 100, count = 0;
-        while (stakePerDay != todayStake / 100 * 50 && stakePerDay != stakePerDay + todayStake / 100 * 50) {
-            gamePoint = random.nextInt(2);
-            count++;
-            System.out.print("Game " + count + ": ");
-            switch (gamePoint) {
-                case WIN:
-                    stakePerDay++;
-                    System.out.println("Gambler WON!");
-                    break;
-                case LOOSE:
-                    stakePerDay--;
-                    System.out.println("Gambler LOOSE!");
+        int todayStake = 100;
+        for (int i = 1; i <= 20; i++) {
+            count = 0;
+            lost = 0;
+            
+            while (stakePerDay != todayStake / 100 * 50 && stakePerDay != todayStake + todayStake / 100 * 50) {
+                gamePoint = random.nextInt(2);
+                count++;
+                switch (gamePoint) {
+                    case WIN:
+                        stakePerDay++;
+                        break;
+                    case LOOSE:
+                        lost++;
+                        stakePerDay--;
+                }
             }
+            System.out.println("Day " + i + ": Total no of game lost: " + lost);
+            System.out.println("Day " + i + ": Total no of game won: " + (count - lost));
+            System.out.println("Day " + i + ": Gambler stake: " + stakePerDay);
+            
+            stakePerDay = 100;
         }
-        System.out.println("Gambler stake after first day played: " + stakePerDay);
     }
 
 }
