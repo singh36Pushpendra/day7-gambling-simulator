2009e267f4e1ecf0e952d0c607b0c8989dc77b77
singh36Pushpendra <psthakur.1999@gmail.com>
2022-10-13 13:39:47 +0530

    Added: UC3 Fifty percent Won or Loose

--- src/gambler/program/GamblingSimulator.java
+++ src/gambler/program/GamblingSimulator.java
@@ -8,24 +8,29 @@ public class GamblingSimulator {
         System.out.println("Welcome to Gambling Simulator Program!");
         final int $PER_GAME = 1, WIN = 1, LOOSE = 0;
         int stakePerDay = 100;
-        
+
         System.out.println("Gambler everyday stake: $" + stakePerDay + "\n"
                 + "Gambler bet every game: $" + $PER_GAME);
-        
+
         Random random = new Random();
-        int gamePoint = random.nextInt(2);
-        
-        switch(gamePoint) {
-            case WIN:
-                stakePerDay++;
-                System.out.println("Gambler WON!");
-                break;
-            case LOOSE:
-                stakePerDay--;
-                System.out.println("Gambler LOOSE!");
+        int gamePoint;
+
+        int todayStake = 100, count = 0;
+        while (stakePerDay != todayStake / 100 * 50 && stakePerDay != stakePerDay + todayStake / 100 * 50) {
+            gamePoint = random.nextInt(2);
+            count++;
+            System.out.print("Game " + count + ": ");
+            switch (gamePoint) {
+                case WIN:
+                    stakePerDay++;
+                    System.out.println("Gambler WON!");
+                    break;
+                case LOOSE:
+                    stakePerDay--;
+                    System.out.println("Gambler LOOSE!");
+            }
         }
-        
-        System.out.println("Gambler stake after first play: " + stakePerDay);
+        System.out.println("Gambler stake after first day played: " + stakePerDay);
     }
-    
+
 }
